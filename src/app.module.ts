09c6e5bcc3681e1530fcd01b5284fb12abc0684a
techguru0411 <techguru0411@gmail.com>
2021-02-04 22:23:31 +0200

    Connected to Cloud DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProduseModule } from './produse/produse.module';
+import { ConfigModule } from 'nestjs-dotenv';
+import { ConfigService } from 'nestjs-dotenv';
+
+MongooseModule.forRoot(process.env.MONGO_DB_URL);
 
 @Module({
-  imports: [ProduseModule],
+  imports: [ProduseModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private readonly configService: ConfigService
+) {}
+}
